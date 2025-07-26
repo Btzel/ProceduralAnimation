f26ed27f0fe53b814e120ec77e136b980a16fa61
Burak Tüzel <buraktsoftware@gmail.com>
2025-07-26 14:30:44 +0300

    v0.1 Gecko Head Tracking Complete

--- ProceduralAnimation/Assets/Scripts/GeckoController.cs
+++ ProceduralAnimation/Assets/Scripts/GeckoController.cs
@@ -2,15 +2,38 @@ using UnityEngine;
 
 public class GeckoController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] Transform _target;
+
+    [SerializeField] Transform _headBone;
+    [SerializeField] float _headBoneRotationSpeed;
+    [SerializeField] float _headMaxTurnAngle;
+
+    void LateUpdate()
     {
-        
+        HeadTrackingUpdate();        
     }
 
-    // Update is called once per frame
-    void Update()
+    void HeadTrackingUpdate()
     {
-        
+        Quaternion currentLocalRotation = _headBone.localRotation;
+        _headBone.localRotation = Quaternion.identity;
+
+        Vector3 targetWorldLookDir = _target.position - _headBone.position;
+        Vector3 targetLocalLookDir = _headBone.InverseTransformDirection(targetWorldLookDir);
+
+        targetLocalLookDir = Vector3.RotateTowards(
+            Vector3.forward,
+            targetLocalLookDir,
+            Mathf.Deg2Rad * _headMaxTurnAngle,
+            0f
+        );
+
+        Quaternion targetLocalRotation = Quaternion.LookRotation(targetLocalLookDir, Vector3.up);
+
+        _headBone.localRotation = Quaternion.Slerp(
+            currentLocalRotation,
+            targetLocalRotation,
+            1 - Mathf.Exp(-_headBoneRotationSpeed * Time.deltaTime)
+        );
     }
 }
