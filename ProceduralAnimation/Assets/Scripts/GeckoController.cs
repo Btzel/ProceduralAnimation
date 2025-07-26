8873de79fa9ba4bd40f71ec90eb0135842e7669d
Burak Tüzel <buraktsoftware@gmail.com>
2025-07-26 14:58:51 +0300

    v0.2 Gecko Eye Tracking Complete

--- ProceduralAnimation/Assets/Scripts/GeckoController.cs
+++ ProceduralAnimation/Assets/Scripts/GeckoController.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class GeckoController : MonoBehaviour
 {
+    [Header("Target")]
     [SerializeField] Transform _target;
 
+    [Header("Head Tracking")]
     [SerializeField] Transform _headBone;
-    [SerializeField] float _headBoneRotationSpeed;
+    [SerializeField] float _headTrackingSpeed;
     [SerializeField] float _headMaxTurnAngle;
+
+    [Header("Eye Tracking")]
+    [SerializeField] Transform _leftEyeBone;
+    [SerializeField] Transform _rightEyeBone;
+    [SerializeField] float _eyeTrackingSpeed;
+    [SerializeField] float _leftEyeMaxYRotation;
+    [SerializeField] float _leftEyeMinYRotation;
+    [SerializeField] float _rightEyeMaxYRotation;
+    [SerializeField] float _rightEyeMinYRotation;
 
     void LateUpdate()
     {
-        HeadTrackingUpdate();        
+        HeadTrackingUpdate();
+        EyeTrackingUpdate();
     }
 
     void HeadTrackingUpdate()
@@ -33,7 +45,61 @@ public class GeckoController : MonoBehaviour
         _headBone.localRotation = Quaternion.Slerp(
             currentLocalRotation,
             targetLocalRotation,
-            1 - Mathf.Exp(-_headBoneRotationSpeed * Time.deltaTime)
+            1 - Mathf.Exp(-_headTrackingSpeed * Time.deltaTime)
         );
     }
+
+    void EyeTrackingUpdate()
+    {
+        Quaternion _targetEyeRotation = Quaternion.LookRotation(
+            _target.position - _headBone.position,
+            transform.up
+        );
+
+        _leftEyeBone.rotation = Quaternion.Slerp(
+            _leftEyeBone.rotation,
+            _targetEyeRotation,
+            1 - Mathf.Exp(-_eyeTrackingSpeed * Time.deltaTime)
+        );
+        _rightEyeBone.rotation = Quaternion.Slerp(
+            _rightEyeBone.rotation,
+            _targetEyeRotation,
+            1 - Mathf.Exp(-_eyeTrackingSpeed * Time.deltaTime)
+        );
+
+        float leftEyeCurrentYRotation = _leftEyeBone.localEulerAngles.y;
+        float rightEyeCurrentYRotation = _rightEyeBone.localEulerAngles.y;
+
+        if (leftEyeCurrentYRotation > 180)
+        {
+            leftEyeCurrentYRotation -= 360;
+        }
+        if (rightEyeCurrentYRotation > 180)
+        {
+            rightEyeCurrentYRotation -= 360;
+        }
+        float leftEyeClampedYRotation = Mathf.Clamp(
+            leftEyeCurrentYRotation,
+            _leftEyeMinYRotation,
+            _leftEyeMaxYRotation
+        );
+
+        float rightEyeClampedYRotation = Mathf.Clamp(
+            rightEyeCurrentYRotation,
+            _rightEyeMinYRotation,
+            _rightEyeMaxYRotation
+        );
+
+        _leftEyeBone.localEulerAngles = new Vector3(
+            _leftEyeBone.localEulerAngles.x,
+            leftEyeClampedYRotation,
+            _leftEyeBone.localEulerAngles.z
+        );
+        _rightEyeBone.localEulerAngles = new Vector3(
+            _rightEyeBone.localEulerAngles.x,
+            rightEyeClampedYRotation,
+            _rightEyeBone.localEulerAngles.z
+        );
+    }
+
 }
